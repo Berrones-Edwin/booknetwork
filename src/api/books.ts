01525f2569cb936ef4687823972fe6661d17314f
Berrones Edwin <Berrones.Edwin@hotmail.com>
2025-11-20 12:32:05 -0600

    add token to the request

--- src/api/books.ts
+++ src/api/books.ts
@@ -1,50 +1,95 @@
 import { apiFetch, BASE_URL } from "./client";
 import type { BookRequest, BookResponse, PageResponseBookResponse, PageResponseBorrowedBookResponse } from "./types/types";
+import { getToken } from "./utils/token";
 
+const token = getToken();
+console.log(token)
 
 export const BooksApi = {
     findAll: (page = 0, size = 10) =>
-        apiFetch<PageResponseBookResponse>(`/books?page=${page}&size=${size}`),
+        apiFetch<PageResponseBookResponse>(`/books?page=${page}&size=${size}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     findById: (bookID: number) =>
-        apiFetch<BookResponse>(`/books/${bookID}`),
+        apiFetch<BookResponse>(`/books/${bookID}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     findAllByOwner: (page = 0, size = 10) =>
         apiFetch<PageResponseBookResponse>(
-            `/books/owner?page=${page}&size=${size}`
+            `/books/owner?page=${page}&size=${size}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }
         ),
 
     findAllBorrowed: (page = 0, size = 10) =>
         apiFetch<PageResponseBorrowedBookResponse>(
-            `/books/borrowed?page=${page}&size=${size}`
+            `/books/borrowed?page=${page}&size=${size}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }
         ),
 
     findAllReturned: (page = 0, size = 10) =>
         apiFetch<PageResponseBorrowedBookResponse>(
-            `/books/returned?page=${page}&size=${size}`
+            `/books/returned?page=${page}&size=${size}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }
         ),
 
     save: (body: BookRequest) =>
         apiFetch<number>("/books", {
             method: "POST",
             body: JSON.stringify(body),
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
         }),
 
     updateShareable: (bookId: number) =>
-        apiFetch<number>(`/books/shareable/${bookId}`, { method: "PATCH" }),
+        apiFetch<number>(`/books/shareable/${bookId}`, {
+            method: "PATCH", headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     updateArchived: (bookId: number) =>
-        apiFetch<number>(`/books/archived/${bookId}`, { method: "PATCH" }),
+        apiFetch<number>(`/books/archived/${bookId}`, {
+            method: "PATCH", headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     borrow: (bookId: number) =>
-        apiFetch<number>(`/books/borrow/${bookId}`, { method: "PATCH" }),
+        apiFetch<number>(`/books/borrow/${bookId}`, {
+            method: "PATCH", headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     returnBorrow: (bookId: number) =>
-        apiFetch<number>(`/books/borrow/return/${bookId}`, { method: "PATCH" }),
+        apiFetch<number>(`/books/borrow/return/${bookId}`, {
+            method: "PATCH", headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }),
 
     approveReturn: (bookId: number) =>
         apiFetch<number>(`/books/borrow/return/apporove/${bookId}`, {
             method: "PATCH",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
         }),
 
     uploadCover: async (bookId: number, file: File): Promise<object> => {
--- src/api/feedback.ts
+++ src/api/feedback.ts
@@ -1,16 +1,25 @@
 import { apiFetch } from "./client";
 import type { FeedBackRequest, PageResponseFeedBackResponse } from "./types/types";
+import { getToken } from "./utils/token";
 
-
+const token = getToken()
 export const FeedbackApi = {
     save: (body: FeedBackRequest) =>
         apiFetch<number>("/feedbacks", {
             method: "POST",
             body: JSON.stringify(body),
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
         }),
 
     findAllByBook: (bookId: number, page = 0, size = 10) =>
         apiFetch<PageResponseFeedBackResponse>(
-            `/feedbacks/book/${bookId}?page=${page}&size=${size}`
+            `/feedbacks/book/${bookId}?page=${page}&size=${size}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }
+
         ),
 };
