6807f7ab46774e6d2766141605bd83f924653654
Berrones Edwin <Berrones.Edwin@hotmail.com>
2025-11-20 12:33:02 -0600

    delete the ? to the fields

--- src/api/types/types.ts
+++ src/api/types/types.ts
@@ -14,88 +14,88 @@ export interface AuthenticationRequest {
 }
 
 export interface AuthenticationResponse {
-    token?: string;
+    token: string;
 }
 
 // ===========================
 // FeedBack
 // ===========================
 export interface FeedBackRequest {
-    note?: number; // 0 - 5
+    note: number; // 0 - 5
     comment: string;
     bookId: number;
 }
 
 export interface FeedBackResponse {
-    note?: number;
-    comment?: string;
-    ownFeedBack?: boolean;
+    note: number;
+    comment: string;
+    ownFeedBack: boolean;
 }
 
 export interface PageResponseFeedBackResponse {
-    content?: FeedBackResponse[];
-    number?: number;
-    size?: number;
-    total?: number;
-    totalPages?: number;
-    first?: boolean;
-    last?: boolean;
+    content: FeedBackResponse[];
+    number: number;
+    size: number;
+    total: number;
+    totalPages: number;
+    first: boolean;
+    last: boolean;
 }
 
 // ===========================
 // Book
 // ===========================
 export interface BookRequest {
-    id?: number;
+    id: number;
     title: string;
     authorName: string;
     isbn: string;
-    synopsis?: string;
-    shareable?: boolean;
+    synopsis: string;
+    shareable: boolean;
 }
 
 export interface BookResponse {
-    title?: string;
-    authorName?: string;
-    isbn?: string;
-    synopsis?: string;
-    owner?: string;
-    cover?: string; // byte[]
-    rate?: number;
-    archived?: boolean;
-    shareable?: boolean;
-    id?: number;
+    title: string;
+    authorName: string;
+    isbn: string;
+    synopsis: string;
+    owner: string;
+    cover: string; // byte[]
+    rate: number;
+    archived: boolean;
+    shareable: boolean;
+    id: number;
 }
 
 export interface PageResponseBookResponse {
-    content?: BookResponse[];
-    number?: number;
-    size?: number;
-    total?: number;
-    totalPages?: number;
-    first?: boolean;
-    last?: boolean;
+    content: BookResponse[];
+    number: number;
+    size: number;
+    total: number;
+    totalPages: number;
+    first: boolean;
+    last: boolean;
 }
 
 // ===========================
 // Borrowed Books
 // ===========================
 export interface BorrowedBookResponse {
-    title?: string;
-    authorName?: string;
-    isbn?: string;
-    rate?: number;
-    returned?: boolean;
-    returnApproved?: boolean;
-    id?: number;
+    title: string;
+    authorName: string;
+    isbn: string;
+    rate: number;
+    returned: boolean;
+    returnApproved: boolean;
+    id: number;
 }
 
 export interface PageResponseBorrowedBookResponse {
-    content?: BorrowedBookResponse[];
-    number?: number;
-    size?: number;
-    total?: number;
-    totalPages?: number;
-    first?: boolean;
-    last?: boolean;
+    content: BorrowedBookResponse[];
+    number: number;
+    size: number;
+    total: number;
+    totalPages: number;
+    first: boolean;
+    last: boolean;
 }
